b20d5550d2ab05db55b7ce08feebde0cd6a48640
Paulo Daniel <paulodanielm19@gmail.com>
2026-02-10 21:57:23 -0300

    att:pesos

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const duvida = document.getElementById('inputPergunta')
 const opcao1 = document.getElementById('opcaoA')
 const opcao2 = document.getElementById('opcaoB')
-const butoes = document.querySelectorAll('.carac');
-
+const criterios = document.querySelectorAll('.carac');
 const adicionar = document.getElementById('btn-adicionar')
-
-
 const lista = document.getElementById('li-decisoes');
 const listaButtons = document.getElementById('listaBtn');
+const pesos = document.getElementById('listPesos')
 
 
-
- let DadosDaPergunta = {
+let DadosDaPergunta = {
   texto: "",
   opcaoA: "",
   opcaoB: ""
@@ -31,24 +28,45 @@ adicionar.addEventListener("click", () => {
         alert("Digite a pergunta");
         return;
     }
+    if(DadosDaPergunta.opcaoA === "" || DadosDaPergunta.opcaoB === ""){
+        alert("Digite as opçoes")
+        return;
+    }
 
     const li = document.createElement("li");
     li.textContent = DadosDaPergunta.texto;
     li.classList.add("criada");
-    
-
     lista.appendChild(li); 
     duvida.value = "";
     opcao1.value = "";
     opcao2.value = "";
+
+});
+
+document.getElementById('li-decisoes').addEventListener('click', function (e) {
+  if (e.target.classList.contains('criada')) {
+    console.log('Cliquei no item criado depois');
+
+    
+  }
 });
 
 
-butoes.forEach(botao => {
+
+criterios.forEach(botao => {
     botao.addEventListener("click", () => {
-        
         botao.classList.toggle("selecionada");
     
+    if(botao.classList.contains("selecionada")){
+        console.log("TESTE")
+        pesos.classList.remove('hidden')
+        
+
+    }
+    else{
+        console.log("saiu")
+        pesos.classList.add('hidden')
+    }
     });
 });
 
